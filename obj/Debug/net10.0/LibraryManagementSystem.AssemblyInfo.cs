289e2a1b5dd8ee1eefbba9ee89ad2519f7e2e149
NguyenPhuongTra <nguyenphuongtra@MacBook-Pro-cua-NguyenPhuongTra.local>
2026-09-05 12:54:10 +0700

    add gitignore

--- obj/Debug/net10.0/LibraryManagementSystem.AssemblyInfo.cs
+++ obj/Debug/net10.0/LibraryManagementSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0388bdd57c616cf6966c98bd6df37a956431a3d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b92d77950cd0fa145d5cd431ffaa34ba0837c4c2")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraryManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
